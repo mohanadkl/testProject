95f4d8ef857a8acef0ee4e9a9970d6bb7d82bca2
mohanadkl <mohanadkouloughli@gmail.com>
2022-08-20 22:28:02 +0200

    first commit

---
+++ color.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std ;
+//add color 
+void affiche(){cout<<"i am a devlopper";}
+void color (){system("color 9C");}
+int main ()
+{
+affiche();
+color();
+
+return 0 ;
+}
